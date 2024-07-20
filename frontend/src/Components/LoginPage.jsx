b618636a46b54168fb362864ddb27732eb0301c3
jmngandu <jamesmngandu@gmail.com>
2024-07-20 15:21:59 +0300

    added frontend

--- frontend/src/Components/LoginPage.jsx
+++ frontend/src/Components/LoginPage.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+
 export default function LoginPage() {
   return (
     <>
-      <h1>Login</h1>
+      <div className="LoginPage">
+        <h1>AQUASENSE AI</h1>
+        <form className="form-section">
+          <div className="email-sect group-log">
+            <h3 className="input-title">Email</h3>
+            <input
+              type="email"
+              placeholder="Gmail account"
+              name="email"
+              className="input"
+              id="input-email"
+            />
+          </div>
+          <div className="password-sect group-log">
+            <h3 className="input-title">Password</h3>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              className="input"
+              id="input-password"
+            />
+          </div>
+
+          <div className="register-input">
+            <button id="login-btn" className="log-btn">
+              Login
+            </button>
+            <button className="log-btn">Register</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
