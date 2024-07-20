594d6a195f37e87b81a51b3af4562b10d6adaadd
jmngandu <jamesmngandu@gmail.com>
2024-07-20 17:35:36 +0300

    added frontend

--- frontend/src/Components/DashboardPage.jsx
+++ frontend/src/Components/DashboardPage.jsx
@@ -7,8 +7,12 @@ export default function DashboardPage() {
         <div className="nav-dash">
           <div className="top-nav">
             <h1>AQUASENSE AI</h1>
-            <h3>Waste locations</h3>
-            <h3>Water Analytics</h3>
+            <h3>
+              <Link>Waste locations</Link>
+            </h3>
+            <h3>
+              <Link>Water Analytics</Link>
+            </h3>
           </div>
           <div className="botton-nav">
             <button className="log-out">
