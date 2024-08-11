5080c8e26941b9b38b89044594652b52e626146c
jmngandu <jamesmngandu@gmail.com>
2024-08-11 20:48:15 +0300

    aquasense

--- frontend/web/src/Components/Account.jsx
+++ frontend/web/src/Components/Account.jsx
@@ -8,7 +8,7 @@ export default function Account() {
       <div className="account-info">
         <i className="fas fa-user-circle fa-3x"></i>
         <div className="more-info">
-          <h3>Chiyembekezo Chilembwe </h3>
+          <h3>JAMES MUKUVI </h3>
 
           <button className="log-out">
             <Link to="/" id="log-out">
